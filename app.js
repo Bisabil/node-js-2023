635b0a1e4f1ac00bae9df241dd18c3fbcec0b35e
Bisabil Nizam] <nbisabil@gmail.com>
2023-01-26 12:05:09 +0800

    Nodemon

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const http = require("http");
+const http = require('http')
+ 
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
+const server = http.createServer((req,res)=> {
+  res.writeHead(200, {"Content-Type": "application/json"})
   res.end(
     JSON.stringify({
-      data: "Hello World!",
+      data: "Hello World!"
     })
-  );
-});
+  )
+})
 
-server.listen(3000);
+console.log(`server is running at http://localhost:3000`)
+
+server.listen(3000)
